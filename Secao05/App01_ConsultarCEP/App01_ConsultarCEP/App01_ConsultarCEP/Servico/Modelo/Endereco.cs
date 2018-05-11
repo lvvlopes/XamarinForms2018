39dda95f4db2f1affe12f36bb1abbd6759b175fc
lvmjd <vagner_lopes@uol.com.br>
2018-05-10 22:37:36 -0300

    Atualização 1

---
+++ Secao05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/Modelo/Endereco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_ConsultarCEP.Servico.Modelo
+{
+    public class Endereco
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string unidade { get; set; }
+        public string ibge { get; set; }
+        public string gia { get; set; }
+    }
+
+    public class Partidas
+    {
+        public string local { get; set; }
+        public int clube_casa_id { get; set; }
+        public int clube_visitante_id { get; set; }
+    }
+}
---
+++ Secao05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            var novoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            var webClient = new WebClient();
+            var conteudo = webClient.DownloadString(novoEnderecoURL);
+
+            var endereco = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return endereco;
+        }
+   
+    }
+}
